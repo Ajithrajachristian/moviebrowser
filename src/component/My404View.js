3e17092d1727858c6607048ce694f001d4452f6a
Ajithrajachristian <88928388+Ajithrajachristian@users.noreply.github.com>
2021-09-26 21:15:18 +0530

    My404View.js

---
+++ src/component/My404View.js
@@ -0,0 +1,10 @@
+import Hero from "./Hero";
+const My404View = (text) => {
+  return (
+    <>
+      <Hero text="Page Not Found" className="pageNotFound" />
+    </>
+  );
+};
+
+export default My404View;
